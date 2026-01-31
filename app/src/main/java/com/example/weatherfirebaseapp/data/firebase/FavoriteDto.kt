63c84e44ce1cd80316716fc85703f87fd4ce3d51
Tulebaeva Marzhan <241398@astanait.edu.kz>
2026-02-01 02:22:14 +0500

    implement FavoriteDto and Firebase CRUD logic

--- app/src/main/java/com/example/weatherfirebaseapp/data/firebase/FavoriteDto.kt
+++ app/src/main/java/com/example/weatherfirebaseapp/data/firebase/FavoriteDto.kt
@@ -1,5 +1,15 @@
 package com.example.weatherfirebaseapp.data.firebase
 
-class FavoriteDto(
+import com.google.firebase.database.IgnoreExtraProperties
 
-)
+@IgnoreExtraProperties
+
+ data class FavoriteDto(
+     val id:String?=null,
+     val cityName:String?=null,
+     val note:String?=null,
+     val createdAt:Long?=null,
+     val createdBy:String?=null
+){
+     constructor():this("","","",null,"")
+ }
--- app/src/main/java/com/example/weatherfirebaseapp/data/firebase/FirebaseService.kt
+++ app/src/main/java/com/example/weatherfirebaseapp/data/firebase/FirebaseService.kt
@@ -1,4 +1,25 @@
 package com.example.weatherfirebaseapp.data.firebase
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
 class FirebaseService {
+    private val auth = FirebaseAuth.getInstance()
+    private val db = FirebaseDatabase.getInstance("https://weatherfirebaseapp-daa35-default-rtdb.europe-west1.firebasedatabase.app/").reference
+
+    fun saveFavoriteCity(cityName: String, note: String) {
+        val uid = auth.currentUser?.uid ?: return
+
+        val favoriteId = db.child("favorites").child(uid).push().key ?: return
+
+        val favorite = FavoriteDto(
+            id = favoriteId,
+            cityName = cityName,
+            note = note,
+            createdAt = System.currentTimeMillis(),
+            createdBy = uid
+        )
+
+        db.child("favorites").child(uid).child(favoriteId).setValue(favorite)
+    }
 }
